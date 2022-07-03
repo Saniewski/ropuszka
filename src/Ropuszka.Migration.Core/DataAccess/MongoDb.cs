52b59a1b41960be2e187a0f8bc6b95246bee11be
Saniewski <pawel.san16@gmail.com>
2022-07-03 17:37:37 +0200

    feat(core): fill missing service methods

--- src/Ropuszka.Migration.Core/DataAccess/MongoDb.cs
+++ src/Ropuszka.Migration.Core/DataAccess/MongoDb.cs
@@ -4,11 +4,11 @@ namespace Ropuszka.Migration.Core.DataAccess;
 
 public static class MongoDb
 {
-    private static readonly string _connectionString = "mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&ssl=false";
+    private const string ConnectionString = "mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&ssl=false";
 
     public static void Put(string collectionName, dynamic document)
     {
-        var client = new MongoClient(_connectionString);
+        var client = new MongoClient(ConnectionString);
         var db = client.GetDatabase("ropuszka");
         var collection = db.GetCollection<dynamic>(collectionName);
         collection.InsertOne(document);
